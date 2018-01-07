82be36c775e4f0020796d12edeb59501c91ba375
sullay <1181518458@qq.com>
2018-01-07 23:39:09 +0800

    sullay

---
+++ element-starter/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+  <el-button @click="xxx">xxxx</el-button>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        checkAll: false
+      };
+    },
+    methods: {
+      handleCheckAllChange(val) {
+        alert("xxxx");
+      },
+      xxx() {
+        this.checkAll=false
+      }
+    }
+  };
+</script>
