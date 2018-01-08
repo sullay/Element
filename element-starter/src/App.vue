7142369120024897f3cc95a124b258c5be33fa87
sullay <1181518458@qq.com>
2018-01-08 17:36:31 +0800

    sullay

--- element-starter/src/App.vue
+++ element-starter/src/App.vue
@@ -1,23 +1,56 @@
 <template>
-  <div>
-  <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
-  <el-button @click="xxx">xxxx</el-button>
-  </div>
+<div>
+<div class="block">
+  <span class="demonstration">默认 click 触发子菜单</span>
+  <el-cascader
+    :options="options"
+    v-model="selectedOptions"
+    @change="handleChange">
+  </el-cascader>
+</div>
+<div class="block">
+  <span class="demonstration">hover 触发子菜单</span>
+  <el-cascader
+    expand-trigger="hover"
+    :options="options"
+    v-model="selectedOptions2"
+    @change="handleChange">
+  </el-cascader>
+</div>
+</div>
 </template>
 <script>
   export default {
-    data() {
+    data () {
       return {
-        checkAll: false
-      };
-    },
-    methods: {
-      handleCheckAllChange(val) {
-        alert("xxxx");
-      },
-      xxx() {
-        this.checkAll=false
+        pickerOptions1: {
+          disabledDate (time) {
+            return time.getTime() > Date.now()
+          },
+          shortcuts: [{
+            text: '今天',
+            onClick (picker) {
+              picker.$emit('pick', new Date())
+            }
+          }, {
+            text: '昨天',
+            onClick (picker) {
+              const date = new Date()
+              date.setTime(date.getTime() - 3600 * 1000 * 24)
+              picker.$emit('pick', date)
+            }
+          }, {
+            text: '一周前',
+            onClick (picker) {
+              const date = new Date()
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+              picker.$emit('pick', date)
+            }
+          }]
+        },
+        value1: '',
+        value2: ''
       }
     }
-  };
+  }
 </script>
